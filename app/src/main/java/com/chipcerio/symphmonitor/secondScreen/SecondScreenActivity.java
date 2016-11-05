9422a2d42c5d1b112e9335bad39a1bb6f62013fb
kenneth <kenneth@sym.ph>
2016-11-05 14:31:57 +0800

    added second screen fixes issue #3

---
+++ app/src/main/java/com/chipcerio/symphmonitor/secondScreen/SecondScreenActivity.java
@@ -0,0 +1,20 @@
+package com.chipcerio.symphmonitor.secondScreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.chipcerio.symphmonitor.R;
+
+/**
+ * Created by kennethlopez on 05/11/2016.
+ */
+
+public class SecondScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_screen_activity);
+    }
+}
